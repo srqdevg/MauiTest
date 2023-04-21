2806f7493da7a8228513ce085b91183f4bf5fe9d
Colin Blakey <78232415+srqdevg@users.noreply.github.com>
2023-04-21 10:10:56 -0400

    Initial Commit

---
+++ MauiTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		BindingContext = this;
+	}
+
+	public string BulletText1 { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+	                                          "tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est " +
+	                                          "ultricies integer quis. Vel fringilla est ullamcorper eget. Urna molestie at " +
+	                                          "elementum eu facilisis sed odio morbi quis. Augue mauris augue neque gravida. ";
+
+	public string BulletText2 { get; set; } = "Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. " +
+	                                          "Sem fringilla ut morbi tincidunt augue. Vitae proin sagittis nisl rhoncus mattis " +
+	                                          "rhoncus urna. Vestibulum sed arcu non odio euismod lacinia at. Egestas congue quisque " +
+	                                          "egestas diam in arcu cursus euismod quis. Sodales ut etiam sit amet.";
+}
+
